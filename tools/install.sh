14144b77142d50ea5c1aa556553f7a0114a95f8b
Charles Guertin <charlesguertin@live.ca>
2023-05-19 20:22:31 -0400

    Change setup for mac

--- tools/install.sh
+++ tools/install.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env bash
 
-# Debian files
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-# APT packages
-sudo apt install -y \
+# useful packages
+brew install \
 	nmap \
 	hydra \
 	wireshark \
 	jq \
 	sqlmap \
-	ncat \
 	binwalk \
-	powershell \
-	checksec
+	powershell
 
 # Create ctf bin directory
 CTF_BIN=$HOME/ctf/bin
@@ -27,18 +20,8 @@ if ! grep -q 'export PATH=$PATH:$HOME/ctf/bin' $HOME/.zshrc; then
 	echo 'export PATH=$PATH:$HOME/ctf/bin' >> $HOME/.zshrc
 fi
 
-# Install IDA free
-if [ ! -f $CTF_BIN/idafree-7.7/ida64 ]
-then
-	wget https://out7.hex-rays.com/files/idafree77_linux.run
-	chmod +x idafree77_linux.run
-	./idafree77_linux.run
-	rm idafree77_linux.run
-	ln -s $CTF_BIN/idafree-7.7/ida64 $CTF_BIN/idafree
-fi
-
 # Web installations
-wget https://portswigger.net/burp/releases/download -O $CTF_BIN/burpsuite # Burp suite
+# wget https://portswigger.net/burp/releases/download -O $CTF_BIN/burpsuite # Burp suite -> download online instead
 wget https://www.benf.org/other/cfr/cfr-0.152.jar -O $CTF_BIN/cfr # Java decompiler
 
 # Install Metasploit
@@ -49,10 +32,6 @@ chmod 755 msfinstall && ./msfinstall && rm msfinstall
 wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.1.4_build/ghidra_10.1.4_PUBLIC_20220519.zip
 unzip ghidra_10.1.4_PUBLIC_20220519.zip -d $CTF_BIN
 rm ghidra_10.1.4_PUBLIC_20220519.zip
-
-# Install geth (ethereum client)
-curl https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.17-25c9b49f.tar.gz | tar -xzf - -C $CTF_BIN
-ln -s $CTF_BIN/geth-linux-amd64-1.10.17-25c9b49f/geth $CTF_BIN/geth
 
 # Go packages
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
--- tools/prerequisites.sh
+++ tools/prerequisites.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-# Install APT packages
-sudo apt update && sudo apt install -y \
+# Install dependencies
+brew install \
 	python3 \
-	python3-pip \
 	curl \
-	git
-
-# Install latest version of Go
-LATEST_GO_VERSION="$(curl -s https://go.dev/VERSION?m=text)"
-echo $LATEST_GO_VERSION
-wget https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
-rm -rf go*
+	git \
