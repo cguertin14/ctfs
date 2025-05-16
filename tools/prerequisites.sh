381052dbb88d87ae9581486ce0bc7e6868d262de
Charles Guertin <charlesguertin@live.ca>
2025-05-16 12:13:05 -0400

    fix: append go binaries location to ~/.zshrc

--- tools/prerequisites.sh
+++ tools/prerequisites.sh
@@ -13,3 +13,7 @@ echo $LATEST_GO_VERSION
 wget https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
 rm -rf go*
+
+if ! grep -q 'export PATH=$PATH:/usr/local/go/bin' $HOME/.zshrc; then
+	echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.zshrc
+fi
