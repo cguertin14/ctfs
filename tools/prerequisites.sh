c377bf2a2ea9f303f308ce989177611a73942dc2
Charles Guertin <charlesguertin@live.ca>
2025-05-16 11:59:40 -0400

    fix: properly parse go version from URL

--- tools/prerequisites.sh
+++ tools/prerequisites.sh
@@ -8,7 +8,7 @@ sudo apt update && sudo apt install -y \
 	git
 
 # Install latest version of Go
-LATEST_GO_VERSION="$(curl -s https://go.dev/VERSION?m=text)"
+LATEST_GO_VERSION="$(curl -s https://go.dev/VERSION?m=text | head -n1)"
 echo $LATEST_GO_VERSION
 wget https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
